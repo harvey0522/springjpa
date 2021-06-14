91b6adb6359bb8e635e890a012bcc5848caa39ae
Annis Zhang <annis.zhang@everbridge.com>
2021-06-15 07:45:10 +0800

    add”

--- src/main/java/com/example/springjpa/SpringjpaApplication.java
+++ src/main/java/com/example/springjpa/SpringjpaApplication.java
@@ -1,6 +1,5 @@
 package com.example.springjpa;
 
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
